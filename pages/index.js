14600d4e58a90a7aaeafd1bd528b4da4bef255ff
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-04-22 08:27:08 +0900

    Layoutコンポーネントを実装

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Head from 'next/head';
-
+import Layout from '../components/Layout';
 export default function Home() {
   return (
-    <div className="min-h-screen py-0 px-2 flex flex-col justify-center items-center">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Hello Nextjs</h1>
-
+    <Layout title="Home">
+      <p className="text-4xl">Welcome to Nextjs</p>
       {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -19,6 +13,6 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer> */}
-    </div>
+    </Layout>
   );
 }
